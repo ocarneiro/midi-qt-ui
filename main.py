5035e487d1dd94fb6892284dba38c1edaebf7068
Otavio Carneiro <git@carneiro.blog.br>
2022-07-04 16:48:47 -0300

    improving readability

--- main.py
+++ main.py
@@ -8,15 +8,20 @@ from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtCore import QObject
 from PySide6.QtCore import Qt  # FindChildOption,FindChildOptions,FindChildrenRecursively,FindDirectChildrenOnly
 
+import rtmidi
 from rtmidi.midiutil import open_midiinput
 
 app = QGuiApplication(sys.argv)
 engine = QQmlApplicationEngine()
 arquivo_qml = os.fspath(Path(__file__).resolve().parent / "main.qml")
 
+midiin = rtmidi.MidiIn()
+print(midiin.get_ports())
+
 
 try:
     midiin, port_name = open_midiinput(0)
+    print("Connected to %s" % port_name)
 except (EOFError, KeyboardInterrupt):
     sys.exit()
 
@@ -27,15 +32,15 @@ class EventoMidiIn(object):
         if controle >= 16 and controle <=19:
             engine.rootObjects()[0].setProperty("valorSlider%d" % controle, valor)
         else:
-            print("gira %s" % mensagem)
+            print("turning %s" % mensagem)
 
 
     def pressiona_tecla(self, mensagem):
-        _, botao, _ = mensagem
+        _, botao, _ = mensagem  # button_value is the second value in message
         engine.rootObjects()[0].setProperty("botao%d"%botao, True)
 
     def solta_tecla(self, mensagem):
-        _, botao, _ = mensagem
+        _, botao, _ = mensagem  # button_value is the second value in message
         engine.rootObjects()[0].setProperty("botao%d"%botao, False)
 
     mapa_comando_metodo = {
